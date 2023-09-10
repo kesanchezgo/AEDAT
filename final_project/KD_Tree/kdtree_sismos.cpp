73fa46c50f02afde2e1666e7165a7adc35a13a8e
Kevin <ksanchezgo@unsa.edu.pe>
2023-09-10 16:25:01 -0500

    magnitude generation added

--- final_project/KD_Tree/kdtree_sismos.cpp
+++ final_project/KD_Tree/kdtree_sismos.cpp
@@ -7,7 +7,8 @@
 
 struct Point {
     double x, y;
-    Point(double x, double y) : x(x), y(y) {}
+    double m;
+    Point(double x, double y, double m) : x(x), y(y), m(m) {}
 };
 
 struct Node {
@@ -119,16 +120,18 @@ int main() {
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> latDist(-16.5000, -16.1000); // Latitud de Arequipa
     std::uniform_real_distribution<> lonDist(-71.7000, -71.2000); // Longitud de Arequipa
+    std::uniform_real_distribution<> magDist(3.0, 7.0); //Magnitud aleatoria entre 3.0 y 7.0
 
     int numPoints = 50; // Cantidad de puntos de ejemplo
     for (int i = 0; i < numPoints; ++i) {
         double lat = latDist(gen);
         double lon = lonDist(gen);
-        tree.insert(Point(lat, lon));
+        double mag = magDist(gen);
+        tree.insert(Point(lat, lon, mag));
     }
 
      // Definir el punto objetivo para la búsqueda de vecinos cercanos
-    Point target(-16.409047, -71.537451); // Utiliza una de las coordenadas de Arequipa como objetivo
+    Point target(-16.409047, -71.537451, 0); // Utiliza una de las coordenadas de Arequipa como objetivo
 
     int k = 5; // Número de vecinos cercanos a buscar
     std::vector<Point> kNearest = tree.findKNearest(target, k);
@@ -141,7 +144,7 @@ int main() {
     std::cout << "Cantidad de temblores cercanos al Centro Historico de Arequipa (-16.409, -71.5375): " << kNearest.size() << std::endl;
     std::cout << "Coordenadas de los " << k << " temblores mas cercanos:" << std::endl;
     for (const Point& nearest : kNearest) {
-        std::cout << "Latitud: " << nearest.x << ", Longitud: " << nearest.y << std::endl;
+        std::cout << "Latitud: " << nearest.x << ", Longitud: " << nearest.y << ", Magnitud: " << nearest.m << std::endl;
     }
 
     return 0;
